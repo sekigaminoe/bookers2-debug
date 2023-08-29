5a8c368207ffe62ffc70b9ce4a791be58d6f2e4d
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-08-29 06:30:19 +0000

    [Finish]応用課題4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+
+    def follows
+      user = User.find(params[:id])
+      @users = user.followings
+    end
+
+    def followers
+      user = User.find(params[:id])
+      @user = user.followers
+    end
   end
 
   private
